4a3f3e7f5b0cea91b4d15b5424b3d0b11d73eecc
hwanseog choi <this0924@gmail.com>
2019-09-16 03:32:43 +0900

    moved schema, test async await and console

---
+++ src/schema.js
@@ -0,0 +1,16 @@
+
+import path from "path";
+import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
+});
+
+export default schema;
+
+//api 폴더의 모든 파일들을 schema 파일에서 한번에 받음
--- src/server.js
+++ src/server.js
@@ -1,23 +1,12 @@
 require("dotenv").config()
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-// define type
-const typeDefs = ` 
-type Query{
-    hello: String!
-}`;
 
-const resolvers = {
-    Query:{
-        hello : () => "Hi"
-    }
-}
-const server = new GraphQLServer({
-    typeDefs, resolvers
-});
+const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev")); //미들웨어 추가
 
