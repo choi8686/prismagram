9306f356be797d381856cdcf0597a81c521fc20c
hwanseog choi <this0924@gmail.com>
2019-09-18 02:17:01 +0900

    sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
-require("dotenv").config()
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+require("dotenv").config();
+
 import {
     GraphQLServer
 } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
+sendSecretMail("this0924@gmail.com", "123")
 const PORT = process.env.PORT || 4000;
 
 
