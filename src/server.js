144d33019aac8e7547f56089ed0b36f0aeb43708
hwanseog choi <this0924@gmail.com>
2019-09-17 23:23:11 +0900

    requestSecret Resolver

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 require("dotenv").config()
-import { GraphQLServer } from "graphql-yoga";
+import {
+    GraphQLServer
+} from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+    schema
+});
 
 server.express.use(logger("dev")); //미들웨어 추가
 
-server.start({port : PORT}, () => console.log(`Server running on port ${PORT}`))
+server.start({
+    port: PORT
+}, () => console.log(`Server running on port ${PORT}`))
