2a94a9b870fd6073a6087cd81c7a0c2fbc0f6342
hwanseog choi <this0924@gmail.com>
2019-09-23 17:58:43 +0900

    searchUser done and fixed syntax error

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -9,8 +9,14 @@ export default {
             const user = await prisma.user({email}); 
             if(user.loginSecret === secret){ 
 
+                await prisma.updateUser({
+                    where: {id: user.id},
+                    data:{
+                        loginSecret: ""
+                    }
+                })
                 return generateToken(user.id) //email과 secret words가 맞으면 토큰생성
-                
+
             } else {
                 throw Error("Wrong email/secret combination");
             }
