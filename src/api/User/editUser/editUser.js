37a37dcdb05c6b4882286d523ad8b2b3d64bb5b1
hwanseog choi <this0924@gmail.com>
2019-09-25 04:44:36 +0900

    me Resolver + Prisma's Limitations

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -1,3 +1,4 @@
+
 import { prisma } from "../../../../generated/prisma-client";
 
 
--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,10 +1,11 @@
+
 import { prisma } from "../../../../generated/prisma-client";
 
-export default{
-    Query: {
-        seeUser: async (_, args, { request }) => {
-            const { id } = args;
-            return await prisma.user({ id })
-        }
+export default {
+  Query: {
+    seeUser: (_, args, { request }) => {
+      const { id } = args;
+      return prisma.user({ id });
     }
-}
+  }
+};
