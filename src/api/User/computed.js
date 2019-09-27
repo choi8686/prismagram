0a5d4066ebb51869cdb0edc2374126145941ffc9
hwanseog choi <this0924@gmail.com>
2019-09-27 22:18:36 +0900

    isLiked computed File

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -11,17 +11,18 @@ export default {
             
         },
         isFollowing: async (parent, _, { request }) => {
+            //parent는 현재 Model을 호출한 객체
             const { user } = request;
             const { id: parentId } = parent;
             try{
                 return prisma.$exists.user({
                     AND: [
                         {
-                            id: user.id
+                            id: user.id // 팔로잉하려는 사람의 ID가 존재하면서
                         },
                         {
                             following_some: {
-                                id: parentId
+                                id: parentId // 그사람의 팔로워에 내가 있는가?
                             }
                         }
                     ]
@@ -31,9 +32,30 @@ export default {
             }
         },
         isSelf: (parent, _, { request }) => {
-            const { user } = request;
-            const {id: parentId } = parent;
+            //check myself
+            const { user } = request; // 현재 로그인한 유저 정보
+            const {id: parentId } = parent; //User를 호출한 Query
             return user.id === parentId;
+        }
+    },
+    Post: {
+        isLiked: (parent, _, { request }) => {
+            const { user } = request;
+            const { id } = parent;
+            return prisma.$exists.like({
+                AND: [
+                    {
+                        user: {
+                            id: user.id
+                        }
+                    },
+                    {
+                        post: {
+                            id
+                        }
+                    }
+                ]
+            })
         }
     }
 };
--- src/api/User/unfollow/unfollow.js
+++ src/api/User/unfollow/unfollow.js
@@ -4,6 +4,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default{
     Mutation:{
         unfollow: async(_, args, { request }) => {
+            //첫번째 인자는 사용하지 않는 args, 두번째 인자는 arguments, 세번째 인자는 context
             isAuthenticated(request);
             const { id } = args;
             const { user } = request;
